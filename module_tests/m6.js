df635606ab83e824c0e944ff34e81e05a9682288
Yaasir <MYN>
2021-08-19 11:23:21 +0400

    Completed module

--- module_tests/m6.js
+++ module_tests/m6.js
@@ -66,12 +66,3 @@ getBooksOrMoviesAsync().then((results) => {
         results
     });
 });
-
-//Create a timer to be cleared later
-const timer1 = setTimeout(() => {console.log("timer 1 has finished")}, 3000);
-
-//Use clearTimeout() to stop a timer
-const timer2 = setTimeout(() => {
-    console.log("timer 2 has finished");
-    clearTimeout(timer1);
-}, 2000);
